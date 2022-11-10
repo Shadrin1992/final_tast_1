5087ac1292f93abe65b2d5ac89907f480f0c8c6a
Evgeny <shadrin_pharm@mail.ru>
2022-11-10 18:32:57 +0300

    Завершили написание программы

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше 3 символов.
+
+string[] array = { "dfjfjw", "-7", "mmm", "dkjfwljf", "fdfk" };
+
+void FilterArray(string[] array)
+{
+    string[] newArray = new string[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[i] = array[i];
+        }
+    }
+
+    var str = string.Join(" ", newArray);
+    Console.WriteLine(str);
+}
+
+FilterArray(array);
